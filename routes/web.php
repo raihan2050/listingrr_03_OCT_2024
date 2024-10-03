2601b49e7808b760da7e581802332f2f02347afa
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-03 14:06:47 +0600

    sidebar clear

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
