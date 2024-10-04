e29db465d6896c62b666f305e87e988084410f63
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-04 23:54:02 +0600

    left dynamic

--- resources/views/back/layouts/components/main-sidebar_admin.blade.php
+++ resources/views/back/layouts/components/main-sidebar_admin.blade.php
@@ -1,13 +1,11 @@
-<ul class="side-menu">
+<ul class="main-menu" style="display: block; margin-left: 0px; margin-right: 0px;">
     <li class="slide">
-        <a class="side-menu__item has-link" href="{{ route('super.dashboard') }}">
+        <a href="index.html" class="side-menu__item">
             <i class="side-menu__icon ri-home-4-line"></i>
             <span class="side-menu__label">@lang('super.dashboard')</span>
         </a>
     </li>
-    <li class="sub-category">
-        <h3>@lang('super.manage_site')</h3>
-    </li>
+    <li class="slide__category"><span class="category-name">@lang('super.manage_site')</span></li>
     <li class="slide">
         <a class="side-menu__item has-link" href="{{ route('super.system.settings') }}">
             <i class="side-menu__icon ri-settings-3-line"></i>
