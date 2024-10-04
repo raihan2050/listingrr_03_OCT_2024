e753bc714b7789b39d87c9c5e6ddd1fcdae64299
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-04 14:14:05 +0600

    Maintenance Lang

--- resources/lang/en/super.php
+++ resources/lang/en/super.php
@@ -3,4 +3,9 @@
 return [
     'welcome' => 'Welcome',
     'goodbye' => 'Goodbye',
+    'dashboard' => 'Dashboard',
+    'manage_site' => 'Manage Site',
+    'maintenance' => 'Maintenance',
+    'off' => 'Off',
+    'on' => 'On',
 ];
--- resources/views/back/layouts/components/main-sidebar_admin.blade.php
+++ resources/views/back/layouts/components/main-sidebar_admin.blade.php
@@ -2,24 +2,24 @@
     <li class="slide">
         <a class="side-menu__item has-link" href="{{ route('super.dashboard') }}">
             <i class="side-menu__icon ri-home-4-line"></i>
-            <span class="side-menu__label">Dashboard</span>
+            <span class="side-menu__label">@lang('super.dashboard')</span>
         </a>
     </li>
     <li class="sub-category">
-        <h3>Manage Site</h3>
+        <h3>@lang('super.manage_site')</h3>
     </li>
     @if (app()->isDownForMaintenance())
         <li class="slide">
             <a class="side-menu__item has-link" href="{{ route('super.maintenance.off') }}">
                 <i class="side-menu__icon ri-cloud-off-line"></i>
-                <span class="side-menu__label">Maintenance Off</span>
+                <span class="side-menu__label">@lang('super.maintenance') @lang('super.off')</span>
             </a>
         </li>
     @else
         <li class="slide">
             <a class="side-menu__item has-link" href="{{ route('super.maintenance.on') }}">
                 <i class="side-menu__icon ri-cloud-line"></i>
-                <span class="side-menu__label">Maintenance On</span>
+                <span class="side-menu__label">@lang('super.maintenance') @lang('super.on')</span>
             </a>
         </li>
     @endif
