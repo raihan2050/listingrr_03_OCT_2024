f828fb063894e7a0cf030f107f976feeda52b70e
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-05 15:10:37 +0600

    Footer

--- resources/views/back/layouts/components/main-footer.blade.php
+++ resources/views/back/layouts/components/main-footer.blade.php
@@ -1,1 +1,16 @@
-<footer class="footer mt-auto py-3 bg-white text-center"> <div class="container"> <span class="text-muted"> Copyright © <span id="year">2024</span> <a href="javascript:void(0);" class="text-dark fw-semibold">Sparic</a>. Designed with <span class="bi bi-heart-fill text-danger"></span> by <a href="javascript:void(0);"> <span class="fw-semibold text-primary text-decoration-underline">Spruko</span> </a> All rights reserved </span> </div> </footer>
+<footer class="footer mt-auto py-3 bg-white text-center">
+    <div class="container">
+        <span class="text-muted">
+            Copyright ©
+            <span id="year">2024</span>
+            <a href="javascript:void(0);" class="text-dark fw-semibold">ListingRR</a>.
+            Designed with
+            <span class="bi bi-heart-fill text-danger"></span>
+            by
+            <a href="javascript:void(0);">
+                <span class="fw-semibold text-primary text-decoration-underline">DeshiServer</span>
+            </a>
+            All rights reserved
+        </span>
+    </div>
+</footer>
