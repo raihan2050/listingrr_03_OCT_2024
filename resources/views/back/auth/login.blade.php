0e97767f2b7cf7681c8caf5d02d42f535a1e3301
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-05 20:43:00 +0600

    fix Login

--- resources/views/back/auth/login.blade.php
+++ resources/views/back/auth/login.blade.php
@@ -1,116 +1,112 @@
-@extends('back.layouts.custom-app1')
+@extends('back.layouts.auth_app')
 
 @section('styles')
 
 @endsection
 
 @section('content')
+    <div class="row justify-content-center align-items-center authentication authentication-basic h-100">
+        <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-6 col-sm-8 col-12">
+            <div class="my-4 d-flex justify-content-center">
+                <a href="#">
+                    <img src="{{asset('build/assets/images/brand-logos/desktop-logo.png')}}" alt="logo" class="desktop-logo">
+                    <img src="{{asset('build/assets/images/brand-logos/desktop-dark.png')}}" alt="logo" class="desktop-dark">
+                </a>
+            </div>
+            <div class="card custom-card">
+                <div class="card-body p-5">
+                    <p class="h5 fw-semibold mb-2 text-center">Sign In</p>
+                    <p class="mb-4 text-muted op-7 fw-normal text-center">Welcome back 🤗!</p>
+                    <form method="POST" action="{{ route('super.login') }}">
+                        @csrf
+                        <div class="row gy-3">
+                            <div class="col-xl-12">
+                                <label for="signinEmail" class="form-label text-default">{{ __('Email Address') }}</label>
+                                <input id="signinEmail"
+                                        type="email"
+                                        class="form-control form-control-lg @error('email') is-invalid @enderror"
+                                        name="email"
+                                        value="{{ old('email') }}"
+                                        placeholder="{{ __('Email Address') }}"
+                                        required
+                                        autocomplete="email"
+                                        autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-xl-12 mb-2">
+                                <label for="signinPassword" class="form-label text-default d-block">
+                                    {{ __('Password') }}
+                                    <a href="javascript:void(0);" class="float-end text-danger">Forget password ?</a>
+                                </label>
+                                <div class="input-group">
+                                    <input id="signinPassword"
+                                            type="password"
+                                            class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                            name="password"
+                                            placeholder="{{ __('Password') }}"
+                                            required
+                                            autocomplete="current-password">
 
-<div class="page-content">
-    <div class="container text-center text-dark">
-        <div class="row">
-            <div class="col-lg-4 d-block mx-auto">
-                <div class="row">
-                    <div class="col-xl-12 col-md-12 col-md-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="text-center mb-2">
-                                    <a class="header-brand1" href="{{url('index')}}">
-                                        <img src="{{asset('build/assets/images/brand/logo.png')}}"
-                                            class="header-brand-img main-logo" alt="Sparic logo">
-                                        <img src="{{asset('build/assets/images/brand/logo-light.png')}}"
-                                            class="header-brand-img darklogo" alt="Sparic logo">
-                                    </a>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    <button class="btn btn-light" type="button" onclick="showHidePassword('signinPassword')" id="button-addon2">
+                                        <i class="ri-eye-off-line align-middle"></i>
+                                    </button>
                                 </div>
-                                <h3>Login</h3>
-                                <p class="text-muted">Sign In to your account</p>
-                                <form method="POST" action="{{ route('super.login') }}">
-                                    @csrf
-                                    <div class="input-group mb-3">
-                                        <span class="input-group-addon bg-white">
-                                            <i class="fa fa-user text-dark"></i>
-                                        </span>
-                                        <input id="email"
-                                                type="email"
-                                                class="form-control @error('email') is-invalid @enderror"
-                                                name="email"
-                                                value="{{ old('email') }}"
-                                                placeholder="{{ __('Email Address') }}"
-                                                required
-                                                autocomplete="email"
-                                                autofocus>
-
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                <div class="mt-2">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label text-muted fw-normal" for="remember">{{ __('Remember Me') }}</label>
                                     </div>
-                                    <div class="input-group mb-4">
-                                        <span class="input-group-addon bg-white">
-                                            <i class="fa fa-unlock-alt text-dark"></i>
-                                        </span>
-                                        <input id="password"
-                                                type="password"
-                                                class="form-control @error('password') is-invalid @enderror"
-                                                name="password"
-                                                placeholder="{{ __('Password') }}"
-                                                required
-                                                autocomplete="current-password">
-
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="row mb-3">
-                                        <div class="col-md-6">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                                <label class="form-check-label" for="remember">
-                                                    {{ __('Remember Me') }}
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <button type="submit" class="btn btn-primary btn-block">
-                                                {{ __('Login') }}
-                                            </button>
-                                        <div>
-                                        @if (Route::has('password.request'))
-                                            <div class="col-12">
-                                                <a class="btn btn-link box-shadow-0 px-0" href="{{ route('password.request') }}">
-                                                    {{ __('Forgot Your Password?') }}
-                                                </a>
-                                            </div>
-                                        @endif
-                                    </div>
-                                </form>
-                                <div class="mt-6 btn-list d-none">
-                                    <button type="button" class="btn btn-icon btn-facebook"><i
-                                            class="fa fa-facebook"></i></button>
-                                    <button type="button" class="btn btn-icon btn-google"><i
-                                            class="fa fa-google"></i></button>
-                                    <button type="button" class="btn btn-icon btn-twitter"><i
-                                            class="fa fa-twitter"></i></button>
-                                    <button type="button" class="btn btn-icon btn-dribbble"><i
-                                            class="fa fa-dribbble"></i></button>
                                 </div>
                             </div>
+                            <div class="col-xl-12 d-grid mt-2">
+                                <button type="submit" class="btn btn-lg btn-primary">
+                                    {{ __('Sign In') }}
+                                </button>
+                            </div>
                         </div>
+                    </form>
+                    <div class="text-center d-none">
+                        <p class="fs-12 text-muted mt-3">
+                            Dont have an account?
+                            <a href="sign-up-basic.html" class="text-primary">Sign Up</a>
+                        </p>
+                    </div>
+                    <div class="text-center my-3 authentication-barrier d-none"> <span>OR</span> </div>
+                    <div class="btn-list text-center d-none">
+                        <button class="btn btn-icon btn-light">
+                            <i class="ri-facebook-line fw-bold text-dark op-7"></i>
+                        </button>
+                        <button class="btn btn-icon btn-light">
+                            <i class="ri-google-line fw-bold text-dark op-7"></i>
+                        </button>
+                        <button class="btn btn-icon btn-light">
+                            <i class="ri-twitter-x-line fw-bold text-dark op-7"></i>
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
 
 @section('scripts')
-
+<script>
+    function showHidePassword(inputId) {
+        const passwordInput = document.getElementById(inputId);
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+        } else {
+            passwordInput.type = "password";
+        }
+    }
+</script>
 @endsection
