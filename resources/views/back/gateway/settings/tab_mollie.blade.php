7b70b8d0b748cd2b0a1fb8443af76b0cc1c115a9
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-12 16:33:19 +0600

    Mollie UI

--- resources/views/back/gateway/settings/tab_mollie.blade.php
+++ resources/views/back/gateway/settings/tab_mollie.blade.php
@@ -1,3 +1,19 @@
 <div class="row">
     @include('back.gateway.settings._common')
+
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+        <div class="mt-4">
+            @include('back.gateway.settings._sendbox')
+        </div>
+    </div>
+
+    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <div class="mt-3">
+            <label for="gatewayKey{{ $gateway['identity'] }}" class="form-label">{{ sprintf(__('super.payc_gateway_key'), $gateway['title']) }}:</label>
+            <input type="text"
+                    class="form-control"
+                    id="gatewayKey{{ $gateway['identity'] }}"
+                    placeholder="{{ sprintf(__('super.payc_gateway_key'), $gateway['title']) }}">
+        </div>
+    </div>
 </div>
