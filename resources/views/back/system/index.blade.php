cc7704fb8fad2580b9a30c666a0b63ada2a2b5d0
Md. Raihan Uddin Ahamed Chowdhury <chowdhuryraihan07@gmail.com>
2024-10-18 12:38:21 +0600

    remove commit

--- resources/views/back/system/index.blade.php
+++ resources/views/back/system/index.blade.php
@@ -18,19 +18,6 @@
                 </ol>
             </nav>
         </div>
-        {{-- <div class="page-title fw-semibold fs-18 mb-0">
-            <div>
-                <a href="javascript:void(0);" class="btn bg-secondary-transparent text-secondary btn-sm" data-bs-toggle="tooltip" title="" data-bs-placement="bottom" data-bs-original-title="Rating">
-                    <span> <i class="fa fa-star"></i> </span>
-                </a>
-                <a href="lockscreen.html" class="btn bg-primary-transparent text-primary mx-2 btn-sm" data-bs-toggle="tooltip" title="" data-bs-placement="bottom" data-bs-original-title="lock">
-                    <span> <i class="fa fa-lock"></i> </span>
-                </a>
-                <a href="javascript:void(0);" class="btn bg-warning-transparent text-warning btn-sm" data-bs-toggle="tooltip" title="" data-bs-placement="bottom" data-bs-original-title="Add New">
-                    <span> <i class="fa fa-plus"></i> </span>
-                </a>
-            </div>
-        </div> --}}
     </div>
 
     <div class="card custom-card">
